abecf4b7c46753cab605ff90d25f71e8ee463124
wwgoncalves <williamcco@gmail.com>
2021-05-06 00:30:23 -0300

    assert using Chai and add more tests

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -3,6 +3,15 @@ class ShoppingCart {
     this._items = items;
   }
 
+  addItem(item) {
+    this._items.push(item);
+    return item;
+  }
+
+  get items() {
+    return this._items;
+  }
+
   get subtotal() {
     return this._items.reduce((accumulatedSubTotal, item) => {
       return accumulatedSubTotal + item.quantity * item.price;
--- src/shoppingCart.spec.js
+++ src/shoppingCart.spec.js
@@ -1,4 +1,4 @@
-const assert = require("assert");
+const { expect } = require("chai");
 const ShoppingCart = require("./shoppingCart");
 
 describe("Shopping Cart", () => {
@@ -9,7 +9,7 @@ describe("Shopping Cart", () => {
       // act
       const subtotal = shoppingCart.subtotal;
       //assert
-      assert.strictEqual(subtotal, 0);
+      expect(subtotal).to.equal(0);
     });
 
     it("should be the sum of the price * quantity for all items", () => {
@@ -31,7 +31,53 @@ describe("Shopping Cart", () => {
         },
       ]);
 
-      assert.strictEqual(shoppingCart.subtotal, 300);
+      expect(shoppingCart.subtotal).to.equal(300);
+    });
+  });
+
+  describe("addItem method", () => {
+    it("should store the item in the shopping cart", () => {
+      const shoppingCart = new ShoppingCart([
+        {
+          id: 3,
+          quantity: 1,
+          price: 40,
+        },
+      ]);
+
+      shoppingCart.addItem({
+        id: 1,
+        quantity: 4,
+        price: 50,
+      });
+
+      expect(shoppingCart.items).to.be.an("array");
+      expect(shoppingCart.items).to.deep.equal([
+        {
+          id: 3,
+          quantity: 1,
+          price: 40,
+        },
+        {
+          id: 1,
+          quantity: 4,
+          price: 50,
+        },
+      ]);
+    });
+
+    it("should return the item that was just stored in the shopping cart", () => {
+      const shoppingCart = new ShoppingCart();
+
+      const item = {
+        id: 3,
+        quantity: 1,
+        price: 40,
+      };
+
+      const addedItem = shoppingCart.addItem(item);
+
+      expect(addedItem).to.equal(item);
     });
   });
 });
